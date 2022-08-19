38894b78416561ecc24f7d4bded24d22e18bccb7
sakmongkhon018 <aucss02@gmail.com>
2022-08-19 21:32:51 +0700

    up

---
+++ lib/list_tile_view.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+class ListTilePage extends StatelessWidget {
+  const ListTilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter ListTile"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+            ListTile(
+              title: Text("Jeff Bezo"),
+              subtitle: Text("Amazon"),
+              leading: Icon(Icons.account_circle,color: Colors.indigo,size: 55,),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete,color: Colors.red),),
+              onTap: () {
+                print("IronMan");
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        child: Icon(Icons.add_a_photo),
+        onPressed: () {
+          print("Menu-1");
+        },
+      ),
+    );
+  }
+}
